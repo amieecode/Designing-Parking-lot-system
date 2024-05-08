b76b88aa16660b1eb3ca920b54c5c0e6fea731c1
keneth origa <66863799+Kenethoriga@users.noreply.github.com>
2024-05-08 12:33:58 +0300

    improve

--- test/test.py
+++ test/test.py
@@ -23,15 +23,23 @@ class TestParkingLot(unittest.TestCase):
         """
         self.parking_lot.create_parking_lot(2, 4)
         car = Vehicle("Car", "KA-01-HB-1234", "Red")
-        self.assertEqual(self.parking_lot.park_vehicle(car), "Parked vehicle. Ticket ID: PR1234_1_4")
+        ticket_id = self.parking_lot.park_vehicle(car)
+        self.assertIsNotNone(ticket_id)
+        self.assertEqual(ticket_id, "PR1234_1_4")
 
         bike = Vehicle("Bike", "KA-01-HB-5678", "Blue")
-        self.assertEqual(self.parking_lot.park_vehicle(bike), "Parked vehicle. Ticket ID: PR1234_1_2")
+        ticket_id = self.parking_lot.park_vehicle(bike)
+        self.assertIsNotNone(ticket_id)
+        self.assertEqual(ticket_id, "PR1234_1_2")
 
         truck = Vehicle("Truck", "KA-01-HB-9012", "Green")
-        self.assertEqual(self.parking_lot.park_vehicle(truck), "Parked vehicle. Ticket ID: PR1234_1_1")
+        ticket_id = self.parking_lot.park_vehicle(truck)
+        self.assertIsNotNone(ticket_id)
+        self.assertEqual(ticket_id, "PR1234_1_1")
 
-        self.assertEqual(self.parking_lot.park_vehicle(car), "Parking Lot Full")
+        with self.assertRaises(Exception) as context:
+            self.parking_lot.park_vehicle(car)
+        self.assertEqual(context.exception.args[0], "Parking Lot Full")
 
     def test_unpark_vehicle(self):
         """
@@ -40,8 +48,9 @@ class TestParkingLot(unittest.TestCase):
         self.parking_lot.create_parking_lot(2, 4)
         car = Vehicle("Car", "KA-01-HB-1234", "Red")
         self.parking_lot.park_vehicle(car)
+        ticket_id = "PR1234_1_4"
 
-        response = self.parking_lot.unpark_vehicle("PR1234_1_4")
+        response = self.parking_lot.unpark_vehicle(ticket_id)
         expected_response = "Unparked vehicle with Registration Number: KA-01-HB-1234 and Color: Red"
         self.assertEqual(response, expected_response)
 
