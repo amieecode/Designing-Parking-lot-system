6b6e3213823e004d8a049f4a0835680a83951ba5
keneth origa <66863799+Kenethoriga@users.noreply.github.com>
2024-05-08 12:39:32 +0300

    improve

--- parking_lot.py
+++ parking_lot.py
@@ -1,24 +1,31 @@
 class Vehicle:
-    def __init__(self, vehicle_type, registration_number, color):
+    def __init__(self, vehicle_type, registration_number, color, speed=0):
         """
         Initialize a new instance of the Vehicle class.
 
         :param str vehicle_type: The type of vehicle (e.g. "car", "truck", "motorcycle")
         :param str registration_number: The registration number of the vehicle
         :param str color: The color of the vehicle
+        :param float speed: The initial speed of the vehicle (in mph)
         """
         self.vehicle_type = vehicle_type  #: The type of vehicle
         self.registration_number = registration_number  #: The registration number of the vehicle
         self.color = color  #: The color of the vehicle
+        self._speed = speed  #: The current speed of the vehicle (in mph)
 
-        # Perform some basic validation on the input
-        if not isinstance(vehicle_type, str) or not isinstance(registration_number, str) or not isinstance(color, str):
+        # Perform basic validation on the input
+        self._validate_input()
+
+    def _validate_input(self):
+        """Validate the input parameters."""
+        if not isinstance(self.vehicle_type, str) or not isinstance(self.registration_number, str) or not isinstance(self.color, str):
             raise TypeError("All arguments must be strings.")
-        if not registration_number or not registration_number.isalnum():
+        if not self.registration_number or not self.registration_number.isalnum():
             raise ValueError("Invalid registration number.")
-
-        # Initialize other attributes
-        self._speed = 0  #: The current speed of the vehicle (in mph)
+        if not isinstance(self._speed, (int, float)):
+            raise TypeError("Speed must be a number.")
+        if self._speed < 0:
+            raise ValueError("Speed cannot be negative.")
 
     @property
     def speed(self):
@@ -33,9 +40,24 @@ class Vehicle:
 
     @speed.setter
     def speed(self, value):
-        if not isinstance(value, (int, float)):
-            raise TypeError("Speed must be a number.")
+        self._validate_input()
         if value < 0:
             raise ValueError("Speed cannot be negative.")
         self._speed = value
+
+    def accelerate(self, amount):
+        """
+        Accelerate the vehicle by a given amount (in mph).
+
+        :param float amount: The amount to accelerate by.
+        """
+        self.speed += amount
+
+    def brake(self, amount):
+        """
+        Brake the vehicle by a given amount (in mph).
+
+        :param float amount: The amount to brake by.
+        """
+        self.speed = max(0, self.speed - amount)
 
