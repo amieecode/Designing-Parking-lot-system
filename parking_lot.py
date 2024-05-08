96e9d10572ba0c5aa9ba24388d3323d7368e91fa
keneth origa <66863799+Kenethoriga@users.noreply.github.com>
2024-05-08 12:32:32 +0300

    comment

--- parking_lot.py
+++ parking_lot.py
@@ -1,4 +1,12 @@
 class Vehicle:
     def __init__(self, vehicle_type, registration_number, color):
-        self.vehicle_type = vehicle_type
+        """
+        Initialize a new instance of the Vehicle class.
 
+        :param vehicle_type: A string representing the type of vehicle (e.g. "car", "truck", "motorcycle")
+        :param registration_number: A string representing the registration number of the vehicle
+        :param color: A string representing the color of the vehicle
+        """
+        self.vehicle_type = vehicle_type  #: The type of vehicle
+        self.registration_number = registration_number  #: The registration number of the vehicle
+        self.color = color  #: The color of the vehicle
