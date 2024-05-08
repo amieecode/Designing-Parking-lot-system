6a4b54ea4b80f41b4f6d533083c8cc463ac47a5b
keneth origa <66863799+Kenethoriga@users.noreply.github.com>
2024-05-08 13:34:58 +0300

    Update parking_lot.py

--- parking_lot.py
+++ parking_lot.py
@@ -1,70 +1,73 @@
 class Vehicle:
-    def __init__(self, vehicle_type, registration_number, color, speed=0):
-        """
-        Initialize a new instance of the Vehicle class.
+    def __init__(self, vehicle_type, registration_number, color):
+        self.vehicle_type = vehicle_type
+        self.registration_number = registration_number
+        self.color = color
 
-        :param str vehicle_type: The type of vehicle (e.g. "car", "truck", "motorcycle")
-        :param str registration_number: The registration number of the vehicle
-        :param str color: The color of the vehicle
-        :param float speed: The initial speed of the vehicle (in mph)
-        """
-        # Assign the input parameters to instance variables
-        self.vehicle_type = vehicle_type  #: The type of vehicle
-        self.registration_number = registration_number  #: The registration number of the vehicle
-        self.color = color  #: The color of the vehicle
-        self._speed = speed  #: The current speed of the vehicle (in mph)
+class ParkingSlot:
+    def __init__(self, slot_type, floor, slot_number):
+        self.slot_type = slot_type
+        self.floor = floor
+        self.slot_number = slot_number
+        self.is_available = True
+        self.vehicle = None
 
-        # Perform basic validation on the input
-        self._validate_input()
+class ParkingLot:
+    def __init__(self, parking_lot_id):
+        self.parking_lot_id = parking_lot_id
+        self.floors = []
 
-    def _validate_input(self):
-        """Validate the input parameters."""
-        if not isinstance(self.vehicle_type, str) or not isinstance(self.registration_number, str) or not isinstance(self.color, str):
-            raise TypeError("All arguments must be strings.")
-        if not self.registration_number or not self.registration_number.isalnum():
-            raise ValueError("Invalid registration number.")
-        if not isinstance(self._speed, (int, float)):
-            raise TypeError("Speed must be a number.")
-        if self._speed < 0:
-            raise ValueError("Speed cannot be negative.")
+    def add_floor(self, no_of_slots):
+        floor = []
+        truck_slot = ParkingSlot('truck', len(self.floors) + 1, 1)
+        floor.append(truck_slot)
+        for i in range(2):
+            bike_slot = ParkingSlot('bike', len(self.floors) + 1, i + 2)
+            floor.append(bike_slot)
+        for i in range(3, no_of_slots + 1):  # assuming no_of_slots per floor
+            car_slot = ParkingSlot('car', len(self.floors) + 1, i + 1)
+            floor.append(car_slot)
+        self.floors.append(floor)
 
-    @property
-    def speed(self):
-        """
-        The current speed of the vehicle (in mph).
+    def find_available_slot(self, vehicle):
+        for floor in self.floors:
+            for slot in floor:
+                if slot.is_available and slot.slot_type == vehicle.vehicle_type:
+                    return slot
+        return None
 
-        :getter: Returns the current speed.
-        :setter: Sets the new speed.
-        :type: float
-        """
-        return self._speed
+    def park_vehicle(self, vehicle):
+        available_slot = self.find_available_slot(vehicle)
+        if available_slot:
+            available_slot.is_available = False
+            available_slot.vehicle = vehicle
+            ticket_id = f"{self.parking_lot_id}_{available_slot.floor}_{available_slot.slot_number}"
+            return ticket_id
+        else:
+            return "Parking Lot Full"
 
-    @speed.setter
-    def speed(self, value):
-        # Validate the input before setting the new speed
-        self._validate_input()
+    def unpark_vehicle(self, ticket_id):
+        floor_number = int(ticket_id.split('_')[1])
+        slot_number = int(ticket_id.split('_')[2])
+        for floor in self.floors:
+            if floor[0].floor == floor_number:
+                for slot in floor:
+                    if slot.slot_number == slot_number:
+                        slot.is_available = True
+                        slot.vehicle = None
+                        return "Unparked vehicle with Registration Number: {} and Color: {}".format(slot.vehicle.registration_number, slot.vehicle.color)
+        return "Invalid Ticket"
 
-        # Check if the new speed is negative
-        if value < 0:
-            raise ValueError("Speed cannot be negative.")
+    def display_free_slots(self, vehicle_type, display_type):
+        for floor in self.floors:
+            if display_type == 'free_count':
+                free_slots = [slot for slot in floor if slot.is_available and slot.slot_type == vehicle_type]
+                print(f"No. of free slots for {vehicle_type} on Floor {floor[0].floor}: {len(free_slots)}")
+            elif display_type == 'free_slots':
+                free_slots = [slot for slot in floor if slot.is_available and slot.slot_type == vehicle_type]
+                print(f"Free slots for {vehicle_type} on Floor {floor[0].floor}: {', '.join(str(slot.slot_number) for slot in free_slots)}")
 
-        # Set the new speed
-        self._speed = value
-
-    def accelerate(self, amount):
-        """
-        Accelerate the vehicle by a given amount (in mph).
-
-        :param float amount: The amount to accelerate by.
-        """
-        # Increase the current speed by the given amount
-        self.speed += amount
-
-    def brake(self, amount):
-        """
-        Brake the vehicle by a given amount (in mph).
-
-        :param float amount: The amount to brake by.
-        """
-        # Set the new speed to the maximum of 0 or the current speed minus the given amount
-        self.speed = max(0, self.speed - amount)
+    def display_occupied_slots(self, vehicle_type):
+        for floor in self.floors:
+            occupied_slots = [slot for slot in floor if not slot.is_available and slot.slot_type == vehicle_type]
+            print(f"Occupied slots for {vehicle_type} on Floor {floor[0].floor}: {', '.
