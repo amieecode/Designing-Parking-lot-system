59deef20e4075d92581ecdc8aee7f9565b6c6344
keneth origa <66863799+Kenethoriga@users.noreply.github.com>
2024-05-08 12:32:37 +0300

    comment

--- test/test.py
+++ test/test.py
@@ -1,18 +1,26 @@
 import unittest
 from service import ParkingLot, Vehicle
 
-
 class TestParkingLot(unittest.TestCase):
     def setUp(self):
+        """
+        Initialize a new ParkingLot object with the given parking lot ID.
+        """
         self.parking_lot = ParkingLot("PR1234")
 
     def test_create_parking_lot(self):
+        """
+        Test the creation of a parking lot with the given number of floors and slots per floor.
+        """
         self.parking_lot.create_parking_lot(2, 3)
         self.assertEqual(len(self.parking_lot.floors), 2)
         for floor in self.parking_lot.floors:
             self.assertEqual(len(floor), 3)
 
     def test_park_vehicle(self):
+        """
+        Test parking a vehicle in the parking lot.
+        """
         self.parking_lot.create_parking_lot(2, 4)
         car = Vehicle("Car", "KA-01-HB-1234", "Red")
         self.assertEqual(self.parking_lot.park_vehicle(car), "Parked vehicle. Ticket ID: PR1234_1_4")
@@ -26,6 +34,9 @@ class TestParkingLot(unittest.TestCase):
         self.assertEqual(self.parking_lot.park_vehicle(car), "Parking Lot Full")
 
     def test_unpark_vehicle(self):
+        """
+        Test unparking a vehicle from the parking lot using the ticket ID.
+        """
         self.parking_lot.create_parking_lot(2, 4)
         car = Vehicle("Car", "KA-01-HB-1234", "Red")
         self.parking_lot.park_vehicle(car)
@@ -39,11 +50,13 @@ class TestParkingLot(unittest.TestCase):
         self.assertEqual(response, expected_response)
 
     def test_display_free_count(self):
+        """
+        Test displaying the number of free slots for a given vehicle type.
+        """
         self.parking_lot.create_parking_lot(2, 3)
         response = self.parking_lot.display_free_count("Truck")
         expected_response = "No. of free slots for Truck: 2"
         self.assertEqual(response, expected_response)
-
 
 if __name__ == '__main__':
     unittest.main()
